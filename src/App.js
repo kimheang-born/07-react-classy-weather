99830fe7ee6f49936a1b149764f2373e1cc2a708
Kimheang <kimheang135@gmail.com>
2023-11-19 13:10:47 +0700

    chore: persist data in localstorage & fix some bugs

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: 'phnom penh',
+    location: '',
     isLoading: false,
     displayLocation: '',
     weather: {},
@@ -42,6 +42,7 @@ class App extends React.Component {
 
   // async fetchWeather() {
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return;
     try {
       this.setState({ isLoading: true });
 
@@ -65,15 +66,31 @@ class App extends React.Component {
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
       );
       const weatherData = await weatherRes.json();
-      this.setState({ weather: weatherData.daily });
+      this.setState({ weather: weatherData?.daily || [] });
     } catch (err) {
-      console.err(err);
+      console.log(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  // useEffect []
+  componentDidMount() {
+    // this.fetchWeather();
+
+    this.setState({ location: localStorage.getItem('location') || '' });
+  }
+
+  // useEffect [location]
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem('location', this.state.location);
+    }
+  }
 
   render() {
     return (
@@ -83,7 +100,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}>Get weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
